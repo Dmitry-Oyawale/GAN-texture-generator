ac9e98f570fc4290ad1ff9a99520b36b2d22d652
Kevin Lai <laikevin279@gmail.com>
2025-12-28 16:00:24 -0500

    frontend

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, send_from_directory, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files)
+
+if __name__ == '__main__':
+    app.run(debug=True)
