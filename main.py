a6c88de341ab739b830ace99b7443571aa9b2398
Kevin Lai <laikevin279@gmail.com>
2025-12-28 16:30:03 -0500

    backend

--- main.py
+++ main.py
@@ -11,5 +11,23 @@ def index():
     files = os.listdir(UPLOAD_FOLDER)
     return render_template('index.html', files=files)
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return redirect(url_for('index'))
+
+@app.route('/download/<path:filename>', methods=['GET', 'POST'])
+def download_file(filename):
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        filename,
+        as_attachment=True
+    )
+
 if __name__ == '__main__':
     app.run(debug=True)
